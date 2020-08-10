97daee886bdb984e24094a269a54442500a63922
gpsuhas20 <gpsuhas20@gmail.com>
2020-08-10 20:28:41 +0530

    cookie-1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');// to encrypt and decrypt cookie
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -27,37 +27,55 @@ connect.then((db)=>
   console.log("Connected Correctly to the server")
 },(err)=>{console.log(err)}
 )
-
+//Signed cookies give time-limited resource access to a set of files, 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
+
+
 function auth(req,res,next)
 {
-  console.log(req.headers);
-  var authHeader=req.headers.authorization;
-
-  if(!authHeader)
+  console.log(req.signedCookies);
+// authentication is done only when the user does not have a cookie.
+  if(!req.signedCookies.user)// means the user is not authenticated. he wont be having a cookie
   {
-    var err=new Error('You are not authenticated')
-    res.setHeader("WWW-Authenticate",'Basic');
-    err.status=401;
-    return next(err);
-  }
-  // auth contains an array of username and password.
-  var auth=new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');// o contains base and 2nd username:password  againn split based on colon
-  var username=auth[0];
-  var password=auth[1];
+    var authHeader=req.headers.authorization;
 
-  if(username==='admin'&& password==="password")
-  {
-    next();
+    if(!authHeader)
+    {
+      var err=new Error('You are not authenticated')
+      res.setHeader("WWW-Authenticate",'Basic');
+      err.status=401;
+      return next(err);
+    }
+    // auth contains an array of username and password.
+    var auth=new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');// o contains base and 2nd username:password  againn split based on colon
+    var username=auth[0];
+    var password=auth[1];
+  
+    if(username==='admin'&& password==="password")
+    {
+      res.cookie('user','admin',{signed:true}) // thats the reason we are checking the user in cookie checker
+      next();
+    }
+    else{
+      var err=new Error('You are not authenticated')
+      res.setHeader("WWW-Authenticate",'Basic');
+      err.status=401;
+      return next(err)
+    }
+
   }
   else{
-    var err=new Error('You are not authenticated')
-    res.setHeader("WWW-Authenticate",'Basic');
-    err.status=401;
-    return next(err)
+    if(req.signedCookies.user==='admin'){
+      next();
+    }
+    else{
+      var err=new Error('You are not authenticated');
+      err.status=401;
+      return next(err);
+    }
   }
 }
 
