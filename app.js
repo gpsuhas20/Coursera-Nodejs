c02aeb65b4524f104a8605fac462c52b83d709b1
gpsuhas20 <gpsuhas20@gmail.com>
2020-08-13 09:00:21 +0530

    https secure

--- app.js
+++ app.js
@@ -30,6 +30,17 @@ connect.then((db) => {
 
 
 var app = express();
+// if its a secure connection in the req object secure will be set to true.
+app.all('*',(req,res,next)=>
+{
+  if(req.secure)
+  {
+    return next();
+  }
+  else{
+    res.redirect(307,"https://"+req.hostname + ':'+app.get('secPort')+ req.url);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
